46b7dea97e69fbd78bc3bc0659a92baab82fc535
Riyan Satria <riyan.satria.619@icloud.com>
2024-06-15 16:30:43 +0700

    Updatean

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     blogUrl: 'https://dailyhotels.id',
-    baseUrl: 'http://127.0.0.1:8000',
-    // baseUrl: 'https://app.promociin.com',
+    // baseUrl: 'http://127.0.0.1:8000',
+    baseUrl: 'https://app.promociin.com',
     primaryColor: '#eb3c7f',
 }
 
